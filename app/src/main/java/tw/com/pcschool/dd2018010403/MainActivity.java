06251d81d5d700db8b0003f1ca14d1d81a9fb8ba
Teacher <shaojung@gmail.com>
2018-01-04 14:20:17 +0800

    Spinner change listener

--- app/src/main/java/tw/com/pcschool/dd2018010403/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/dd2018010403/MainActivity.java
@@ -2,19 +2,24 @@ package tw.com.pcschool.dd2018010403;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinner;
     ArrayList<String> data;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = (Spinner) findViewById(R.id.spinner);
+        tv = (TextView) findViewById(R.id.textView);
         data = new ArrayList<>();
         data.add("AA11");
         data.add("BB22");
@@ -23,7 +28,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
                 android.R.layout.simple_list_item_1, data);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                tv.setText(data.get(i));
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
     }
 }
